b414d5d27234d8bcb1801dc3ba60485a5267d9f5
Dnaidn <rearearearearea12@gmail.com>
2026-05-05 19:16:38 +0700

    Refactor payment processing and send Telegram message

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-function copyRek(){
-    let rek = document.getElementById("rek").innerText;
-    navigator.clipboard.writeText(rek);
-    document.getElementById("status").innerText = "✔ Rekening disalin";
-}
+function kirim(){
 
-function goNext(){
-    let status = document.getElementById("status");
-    status.innerText = "⏳ Memproses pembayaran...";
+    let nama = document.getElementById("nama").value;
+    let jumlah = document.getElementById("jumlah").value;
+    let metode = document.getElementById("metode").value;
 
-    setTimeout(()=>{
-        window.location.href = "success.html";
-    },1500);
+    // 🔥 GANTI INI
+    let token = "8646361372:AAFxdpEs_KGHb63pBoZTHhR7PCYAZfWpEBA";
+    let chat_id = "7558232474";
+
+    let text = `
+💳 ORDER BARU
+----------------
+👤 Nama: ${nama}
+💰 Jumlah: Rp ${jumlah}
+🏦 Metode: ${metode}
+`;
+
+    fetch(`https://api.telegram.org/bot${8646361372:AAFxdpEs_KGHb63pBoZTHhR7PCYAZfWpEBA}/sendMessage`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            chat_id: chat_id,
+            text: text
+        })
+    });
+
+    document.getElementById("status").innerText = "✔ Terkirim ke Telegram";
 }
